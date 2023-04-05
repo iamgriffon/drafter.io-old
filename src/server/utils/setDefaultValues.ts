f584008585a8fec0c7f7a00ffffc3d41a2efb93c
iamgriffon <guusilveira@gmail.com>
2023-04-05 04:50:13 -0300

    hotfix

--- src/server/utils/setDefaultValues.ts
+++ src/server/utils/setDefaultValues.ts
@@ -4,34 +4,64 @@ import { GameSeries } from "@/context/MenuContext";
 export const DEFAULT_GAME_TYPE: GameSeries = [{ game: 1, winner: "not" }];
 export const DEFAULT_RED_SIDE_DRAFT_STATE: Draft = {
   picks: [
-    { position: "R1", champion: { name: "", image: "" } },
-    { position: "R2", champion: { name: "", image: "" } },
-    { position: "R3", champion: { name: "", image: "" } },
-    { position: "R4", champion: { name: "", image: "" } },
-    { position: "LAST PICK", champion: { name: "", image: "" } },
+    { position: "R1", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "R2", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "R3", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "R4", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "LAST PICK", champion: { name: "", image: "" , id: "", draftable: true} },
   ],
   bans: [
-    { position: "L1", champion: { name: "", image: "" } },
-    { position: "L2", champion: { name: "", image: "" } },
-    { position: "L3", champion: { name: "", image: "" } },
-    { position: "L4", champion: { name: "", image: "" } },
-    { position: "LAST BAN", champion: { name: "", image: "" } }
-  ]
+    { position: "L1", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "L2", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "L3", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "L4", champion: { name: "", image: "" , id: "", draftable: true} },
+    { position: "LAST BAN", champion: { name: "", image: "" , id: "", draftable: true} },
+  ],
 };
 
 export const DEFAULT_BLUE_SIDE_DRAFT_STATE: Draft = {
   picks: [
-    { position: "FIRST PICK", champion: { name: "", image: "" } },
-    { position: "B2", champion: { name: "", image: "" } },
-    { position: "B3", champion: { name: "", image: "" } },
-    { position: "B4", champion: { name: "", image: "" } },
-    { position: "B5", champion: { name: "", image: "" } },
+    {
+      position: "FIRST PICK",
+      champion: { name: "", image: "", id: "", draftable: true},
+    },
+    {
+      position: "B2",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+    {
+      position: "B3",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+    {
+      position: "B4",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+    {
+      position: "B5",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
   ],
   bans: [
-    { position: "FIRST BAN", champion: { name: "", image: "" } },
-    { position: "F2", champion: { name: "", image: "" } },
-    { position: "F3", champion: { name: "", image: "" } },
-    { position: "F4", champion: { name: "", image: "" } },
-    { position: "F5", champion: { name: "", image: "" } },
-  ]
+    {
+      position: "FIRST BAN",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+    {
+      position: "F2",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+    {
+      position: "F3",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+    {
+      position: "F4",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+    {
+      position: "F5",
+      champion: { name: "", image: "", id: "", draftable: true },
+    },
+  ],
 };
