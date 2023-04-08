abc43f012eeb22e0eb6255eedd2439c0096c2572
iamgriffon <guusilveira@gmail.com>
2023-04-07 21:34:37 -0300

    Removing white button from stage mode

--- src/components/Menu/WinnerPicker.tsx
+++ src/components/Menu/WinnerPicker.tsx
@@ -1,26 +1,28 @@
 import { useMenu } from "@/context/MenuContext";
 
 export function WinnerPicker() {
+  const { setWinnerTeam, stageMode } = useMenu();
 
-  const { setWinnerTeam } = useMenu();
- 
-    return (
-      <div className="max-xl:absolute md:static lg:static sm:static pl-3 left-[76%] mt-1 flex gap-3 items-center justify-start">
-        <button
-          className="bg-red-600 h-10 w-10 rounded-full focus:border-2 focus:border-gray-200"
-          onClick={() => setWinnerTeam('red')}
-        ></button>
-        |
-        <button
-          className="bg-blue-600 h-10 w-10 rounded-full focus:border-2 focus:border-gray-200"
-          onClick={() => setWinnerTeam('blue')}
-        ></button>
-        |
-        <button
-          className="bg-white h-10 w-10 rounded-full focus:border-2 focus:border-gray-200"
-          onClick={() => setWinnerTeam('not')}
-        ></button>
-      </div>
-    );
-  }
-
+  return (
+    <div className="max-xl:absolute md:static lg:static sm:static pl-3 left-[76%] mt-1 flex gap-3 items-center justify-start">
+      <button
+        className="bg-red-600 h-10 w-10 rounded-full focus:border-2 focus:border-gray-200"
+        onClick={() => setWinnerTeam("red")}
+      ></button>
+      |
+      <button
+        className="bg-blue-600 h-10 w-10 rounded-full focus:border-2 focus:border-gray-200"
+        onClick={() => setWinnerTeam("blue")}
+      ></button>
+      {!stageMode && (
+        <>
+          |
+          <button
+            className="bg-white h-10 w-10 rounded-full focus:border-2 focus:border-gray-200"
+            onClick={() => setWinnerTeam("not")}
+          ></button>
+        </>
+      )}
+    </div>
+  );
+}
